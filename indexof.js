c1e3ff90b0fb68fa4c185bd303729a662d702055
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2023-06-26 16:10:17 +0300

    Update indexof.js

--- indexof.js
+++ indexof.js
@@ -2,6 +2,8 @@ function mutation(arr) {
   let secondelem = arr[1].toLowerCase();
   let firstelem = arr[0].toLowerCase();
 
+  //Here we convert the strings to lowercase first for uniformity.
+
   for(let i=0; i<secondelem.length;i++){
     if(firstelem.indexOf(secondelem[i])<0)
       return false;
@@ -9,6 +11,8 @@ function mutation(arr) {
       return true;
     }
   
-  
+  //The we iterate throught the second string, if any of them is not found we return false.
+
+//If they are all found, the loop will finish without returning anything and we get to return true.
 
 mutation(["hello", "hey"]);
